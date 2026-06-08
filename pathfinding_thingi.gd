3875f661fa2d2a1f7d7f04633c7898ac721a2094
PS204coder <psulej10@gmail.com>
2026-06-08 22:06:43 +0200

    Changed the spears movement speed growth system to a parabola

--- pathfinding_thingi.gd
+++ pathfinding_thingi.gd
@@ -4,7 +4,7 @@ var movement_speed = 2500
 
 @export var Goal: Node
 
-
+@export var speed_growth_slow_down: int
 
 func _ready() -> void:
 	scale = Vector2(0.5,0.5)
@@ -15,11 +15,10 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	if scale.x < 1:
 		scale = Vector2(move_toward(scale.x, 1, 0.1),move_toward(scale.y, 1, 0.1) )
-	movement_speed += 1
-	if Global.time_passed == 30:
-		movement_speed += 100
-	if Global.time_passed == 60:
-		movement_speed += 100
+	movement_speed += 1 * ((Global.time_passed* Global.time_passed) - Global.time_passed*speed_growth_slow_down) * delta
+	print(movement_speed)
+
+	
 	
 	
 	
